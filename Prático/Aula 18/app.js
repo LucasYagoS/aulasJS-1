b65a10a0b8de6ad9ba78ad95ba41a358453d0abb
Lucas Yago <lucasyago2010@gmail.com>
2023-03-29 22:56:22 -0300

    aula18

--- Prático/Aula 18/app.js
+++ Prático/Aula 18/app.js
@@ -34,13 +34,13 @@ getPosts()
 const listQuestions = (posts) => {
   const questionsContent = document.getElementById("listQuestions")
 
-
+  let aux = 1
   posts.forEach(post => {
     questionsContent.innerHTML += `
     <div class="card mb-3">
     <div class="card-body d-flex justify-content-between">
         <div>
-            ${post.title}
+            ${aux}. ${post.title}
         </div>
         <div class="d-flex">
             <button type="button" class="button" onclick="editQuestion(${post.id})"><i class="fa-solid fa-pen-to-square fa-lg"></i></button>
@@ -48,6 +48,7 @@ const listQuestions = (posts) => {
         </div>
     </div>
     `
+    aux++
   });
 }
 
@@ -80,32 +81,87 @@ const editQuestion = async (id) => {
   document.querySelector(`input[id = 'flexRadio${currentQuestion.correct}']`).checked = true
 
   return currentId = currentQuestion.id
-}   
+}
 
 
 
-const saveQuestion = async (currentId) =>{
-    const currentQuestion = await getQuestion(currentId)
-    const updatedQuestion = {
-      "title": document.getElementById("questionTitle").value,
-      "alt1": document.getElementById("alternative1").value,
-      "alt2": document.getElementById("alternative2").value,
-      "alt3": document.getElementById("alternative3").value,
-      "alt4": document.getElementById("alternative4").value,
-      "correct": document.querySelector('input[name="flexRadioDefault"]:checked').value
-    }
+const saveQuestion = async (currentId) => {
+  const currentQuestion = await getQuestion(currentId)
+  const updatedQuestion = {
+    "title": document.getElementById("questionTitle").value,
+    "alt1": document.getElementById("alternative1").value,
+    "alt2": document.getElementById("alternative2").value,
+    "alt3": document.getElementById("alternative3").value,
+    "alt4": document.getElementById("alternative4").value,
+    "correct": document.querySelector('input[name="flexRadioDefault"]:checked').value
+  }
 
-  await updateQuestion(currentId , updatedQuestion)
+  await updateQuestion(currentId, updatedQuestion)
   closeModal()
 }
 
 const updateQuestion = async (id, question) => {
   await fetch(`http://localhost:3000/posts/${id}`, {
-      method: "PUT",
-      headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(question)
+    method: "PUT",
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(question)
   })
+}
+
+const deleteQuestion = async (id) => {
+  await fetch(`http://localhost:3000/posts/${id}`, {
+    method: 'DELETE'
+  })
+  getQuestions()
+}
+
+let quizCurrentQuestion = 1
+const prevBtn = document.getElementById("prevBtn")
+const nextBtn = document.getElementById("nextBtn")
+const prevQuestion = async () => {
+
+  if (quizCurrentQuestion - 1 === 1) {
+
+    prevBtn.style.display = "none"
+  } else {
+    quizCurrentQuestion--
+    await quizQuestions(quizCurrentQuestion)
+  }
+}
+
+const countApiSize = async () => {
+  const apiQuestions = await getPosts()
+  let apiCounter = 0
+
+  apiQuestions.forEach((question) => {
+    apiCounter++
+  })
+  return apiCounter
+}
+const nextQuestion = async (apiCounter) => {
+
+  if (quizCurrentQuestion + 1 === apiCounter) {
+
+    nextBtn.style.display = "none"
+    
+  } else {
+    quizCurrentQuestion++
+    await quizQuestions(quizCurrentQuestion)
+    prevBtn.style.display = "block"
+  }
+}
+
+const quizQuestions = async (quizCurrentQuestion) => {
+  const currentQuestion = await getQuestion(quizCurrentQuestion)
+  document.getElementById("questionTitle").innerText = currentQuestion.title
+  document.getElementById("label1").innerText = currentQuestion.alt1
+  document.getElementById("label2").innerText = currentQuestion.alt2
+  document.getElementById("label3").innerText = currentQuestion.alt3
+  document.getElementById("label4").innerText = currentQuestion.alt4
+  const res = document.querySelector('input[name="flexRadioDefault"]:checked').value
+
+
 }
